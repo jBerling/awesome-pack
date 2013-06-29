d0e32985e2622c79c369765dbbe258f2dc9dd954
Jonasson <j.jonasson@gmail.com>
2013-06-29 21:33:02 +0200

    added M-SPC as a prefix

--- config/bindings.el
+++ config/bindings.el
@@ -3,3 +3,10 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(define-prefix-command 'space-map)
+
+(define-key global-map (kbd "M-SPC") 'space-map)
+(define-key global-map (kbd "M-SPC M-SPC") 'live-delete-whitespace-except-one)
+
+;; paredit
