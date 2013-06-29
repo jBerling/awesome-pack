feb4e99231cbc8db1c77ed581a875291d3c913f0
Jonasson <j.jonasson@gmail.com>
2013-06-29 15:45:46 +0200

    Switched from alt to fn as meta key

---
+++ config/awesome-conf.el
@@ -0,0 +1,4 @@
+;; Use fn as meta
+(custom-set-variables
+ '(ns-alternate-modifier (quote none))
+ '(ns-function-modifier (quote meta)))
---
+++ info.el
@@ -0,0 +1,3 @@
+(live-pack-name "Johan-pack")
+(live-pack-version "0.0.1alpha")
+(live-pack-description "My own pack!")
