5b98fab3552620775e70a6db06bd417732715b2e
Johan Jonasson <johan.jonasson@timezynk.com>
2013-07-03 10:31:36 +0200

    added functions to kill all or other buffers

--- config/awesome-conf.el
+++ config/awesome-conf.el
@@ -7,8 +7,20 @@
 
     (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 
-    (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+    ;(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 
     (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 
-    (setq scroll-step 1) ;; keyboard scroll one line at a time
+    (setq scroll-step 3) ;; keyboard scroll one line at a time
+
+(defun kill-all-buffers ()
+  "kill all buffers"
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+
+(defun kill-other-buffers ()
+  "kill all other buffers"
+  (interactive)
+  (mapc 'kill-buffer
+        (delq (current-buffer)
+              (remove-if-not 'buffer-file-name (buffer-list)))))
