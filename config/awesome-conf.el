cda035ca5db4b0183300982a5da13b93580368b3
Jonasson <j.jonasson@gmail.com>
2013-08-28 22:39:17 +0200

    added right alt as meta and right cmd as control

--- config/awesome-conf.el
+++ config/awesome-conf.el
@@ -1,17 +1,39 @@
-;; Use fn as meta
+;;;;;;;;;;;; Modifier keys ;;;;;;;;;;;;
+;;
+;; Below a list of the modifier key names:
+;;
+;; ns-alternate-modifier
+;; ns-command-modifier
+;; ns-control-modifier
+;; ns-function-modifier
+;; ns-option-modifier (just a different name for ns-alternate-modifier)
+;; ns-right-alternate-modifier
+;; ns-right-command-modifier
+;; ns-right-control-modifier
+;; ns-right-option-modifier
+
+;; Use fn and right alt as meta, ctrl and right cmd and control
 (custom-set-variables
- '(ns-alternate-modifier (quote none))
- '(ns-function-modifier (quote meta)))
+ '(ns-alternate-modifier       (quote none))
+ '(ns-function-modifier        (quote meta))
+ '(ns-right-alternate-modifier (quote meta))
+ '(ns-right-command-modifier   (quote control)))
+
+
+;;;;;;;;;;; Scrolling ;;;;;;;;;;;
 
 ;; scroll one line at a time (less "jumpy" than defaults)
 
-    (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ; one line at a time
 
-    ;(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+;;(setq mouse-wheel-progressive-speed nil) ; don't accelerate scrolling
 
-    (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq mouse-wheel-follow-mouse 't) ; scroll window under mouse
 
-    (setq scroll-step 3) ;; keyboard scroll one line at a time
+(setq scroll-step 3) ; keyboard scroll one line at a time
+
+
+;;;;;;;;;; Misc ;;;;;;;;;;;;;
 
 (defun kill-all-buffers ()
   "kill all buffers"
